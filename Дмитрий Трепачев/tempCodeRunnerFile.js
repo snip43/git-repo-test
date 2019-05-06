72d0ac56f92be06237c0884ac5fe332e575579f3
snip43 <rko43@mail.ru>
2019-05-06 17:35:29 +0300

    qq

--- Дмитрий Трепачев/tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-function playerOne(arr) {
-    arr.forEach(function(elem) {
-        elem.style.cursor = 'pointer';
-        elem.addEventListener('click', function() {
-            this.classList.add('crest');
-        });
-    });
-}
