044444795892b2d76f7724b4517e35d4dd628900
snip43 <rko43@mail.ru>
2019-05-25 14:54:27 +0300

    реализованы кнопки Tab,Backspace.Цвет при наведении мышки

--- ЭкраннаяКлавиатура/script.js
+++ ЭкраннаяКлавиатура/script.js
@@ -1,15 +1,34 @@
-const button = document.querySelectorAll('.button'),
+const classButton = document.querySelectorAll('.button'),
+    button = document.querySelectorAll('button'),
     textPlace = document.querySelector('textarea'),
     buttonClear = document.querySelector('#clearButton'),
-    buttonSpace = document.querySelector('.spaceButton');
+    buttonSpace = document.querySelector('.spaceButton'),
+    buttonBackspace = document.querySelector('#backspace'),
+    buttonTab = document.querySelector('#tab');
 
-button.forEach(function(elem) {
+classButton.forEach(function(elem) {
     elem.addEventListener('click', function() {
-
         textPlace.innerHTML += this.innerHTML;
     });
 });
 
+button.forEach(function(elem) {
+    elem.addEventListener('mousemove', function() {
+        elem.style.background = '#ff8080';
+    });
+    elem.addEventListener('mouseout', function() {
+        elem.style.background = '';
+    });
+});
+
+buttonBackspace.addEventListener('click', function() {
+    let text = textPlace.innerHTML.slice(0, -1);
+    textPlace.innerHTML = text;
+});
+
+buttonTab.addEventListener('click', function() {
+    textPlace.innerHTML += '  ';
+});
 
 buttonClear.addEventListener('click', function() {
     textPlace.innerHTML = '';
