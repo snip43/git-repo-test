b0be04f191a29694b54b713e9d4b50005121b6b5
snip43 <rko43@mail.ru>
2019-05-25 09:46:49 +0300

    1

--- Дмитрий Трепачев/CurrentDir/script.js
+++ Дмитрий Трепачев/CurrentDir/script.js
@@ -10,16 +10,16 @@ const table = document.querySelector('#table'),
     name = document.querySelector('#name'),
     price = document.querySelector('#price'),
     quantity = document.querySelector('#quantity'),
-    button = document.querySelector('#button');
-
+    button = document.querySelector('#button'),
+    p = document.querySelector('#totalP');
 start(table, totalP, name, price, quantity, button);
 
 const table2 = document.querySelector('#table2'),
     name2 = document.querySelector('#name2'),
     price2 = document.querySelector('#price2'),
     quantity2 = document.querySelector('#quantity2'),
-    button2 = document.querySelector('#button2');
-
+    button2 = document.querySelector('#button2'),
+    p2 = document.querySelector('#totalP2');
 start(table2, totalP2, name2, price2, quantity2, button2);
 
 function start(tableField, output, nameField, priceField, quantityField, buttonField) {
@@ -29,11 +29,12 @@ function start(tableField, output, nameField, priceField, quantityField, buttonF
         createTd(nameField.value, tr);
         createTd(priceField.value, tr);
         createTd(quantityField.value, tr);
-        createTd(quantityField.value * priceField.value, tr, 'cost');
-        delProductForTable(table, tr);
+        createTdd(quantityField.value * priceField.value, tr, 'cost');
+        delProductForTable(tableField, tr);
 
         tableField.appendChild(tr);
         getSum(tableField, output);
+        editTd(tableField, output, priceField, quantityField);
     });
 }
 
@@ -49,18 +50,23 @@ function delProductForTable(table, tr) {
 
     del.addEventListener('click', function() {
         table.removeChild(tr);
+        editTd();
     });
 
 }
 
-function createTd(text, parent, className) {
+function createTd(text, parent) {
     const td = document.createElement('td');
     td.innerHTML = text;
+    td.classList.add('tdd');
+    parent.appendChild(td);
+}
 
-    if (className != undefined) {
-        td.classList.add(className);
-        parent.appendChild(td);
-    }
+function createTdd(text, parent, className) {
+    const td = document.createElement('td');
+    td.innerHTML = text;
+    td.classList.add(className);
+    td.classList.remove('tdd');
     parent.appendChild(td);
 }
 
@@ -73,4 +79,39 @@ function getSum(tableField, output) {
         output.innerHTML = 'Итого куплено товаров на сумму:' + total;
     });
     return total;
+}
+
+function editTd(tableField, output, priceField, quantityField) {
+    const tdd = tableField.querySelectorAll('.tdd');
+
+    tdd.forEach(function(elem) {
+        elem.addEventListener('click', function() {
+            const newInp = document.createElement('input');
+            newInp.classList.add('editInp');
+            newInp.value = this.innerHTML;
+            console.log(this.innerHTML);
+            table.appendChild(newInp);
+
+            let self = this;
+
+            newInp.addEventListener('blur', function() {
+
+                // createTdd(quantityField.value * priceField.value,tr, 'cost');
+
+                const costs = tableField.querySelectorAll('.cost');
+                let total = 0;
+
+                self.innerHTML = newInp.value;
+                newInp.style.display = 'none';
+
+                costs.forEach(function(item) {
+                    total += Number(item.innerHTML);
+                    output.innerHTML = 'Итого куплено товаров на сумму:' + total;
+                });
+                console.log(total);
+                return total;
+            });
+        });
+    });
+
 }
