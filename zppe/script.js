e22905fe7d5490d7ad4c15f4bc75d36667756406
snip43 <rko43@mail.ru>
2019-06-02 20:25:15 +0300

    реализован простой интерфейс+ расчет с коэф.1

---
+++ zppe/script.js
@@ -0,0 +1,56 @@
+const radioBtnEightTen = document.querySelector('#checkEightTenth'),
+    radioBtnOne = document.querySelector('#checkOne'),
+    radioBtnOneSecond = document.querySelector('#checkOneSecond'),
+
+    basePrice1 = document.querySelector('#basePrice1'),
+    basePrice2 = document.querySelector('#basePrice2'),
+    basePrice3 = document.querySelector('#basePrice3'),
+    basePrice4 = document.querySelector('#basePrice4'),
+
+    numberImBP = document.querySelector('#numberImBP'),
+    numberImP = document.querySelector('#numberImP'),
+    B2B = document.querySelector('#B2B'),
+    orders = document.querySelector('#orders'),
+
+    oklad = document.querySelector('#oklad'),
+    premiya = document.querySelector('#premiya'),
+
+    inputResult = document.querySelector('#result'),
+    calcZP = document.querySelector('#calc');
+
+calcZP.addEventListener('click', getFinishZp);
+
+function getFinishZp() {
+    // console.log('ИМСПримеркой: ' + getPrice(basePrice1, numberImBP));
+    // console.log('ИМБезПримерки: ' + getPrice(basePrice2, numberImP));
+    // console.log('Почта: ' + getPrice(basePrice3, B2B));
+    // console.log('Заказы: ' + getPrice(basePrice4, orders));
+    // console.log('oklad: ' + parseInt(getPrice(oklad)));
+    // console.log('премия: ' + parseInt(getPrice(premiya)));
+    // console.log('сумма: ' + getSum());
+    // console.log('налог 13%: ' + getTax());
+
+    inputResult.value = getFullSum() - getTax();
+}
+
+function getTax() {
+    return Math.floor((parseInt(getPrice(oklad)) + parseInt(getPrice(premiya)) + getSum()) / 100 * 13);
+}
+
+function getFullSum() {
+    return parseInt(getPrice(oklad)) + parseInt(getPrice(premiya)) + getSum();
+}
+
+
+function getSum() {
+    let res = getPrice(basePrice1, numberImBP) + getPrice(basePrice2, numberImP) + getPrice(basePrice3, B2B) + getPrice(basePrice4, orders);
+    return res;
+}
+
+function getPrice(num1, num2) {
+    if (num2) {
+        return parseInt(num1.value) * parseInt(num2.value);
+    } else {
+        return parseInt(num1.value);
+    }
+}
