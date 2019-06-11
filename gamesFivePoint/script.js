db40e0eaf38b54d5799831f4e016853a2a484b4c
snip43 <rko43@mail.ru>
2019-06-11 17:49:59 +0300

    create gamer

--- gamesFivePoint/script.js
+++ gamesFivePoint/script.js
@@ -1,7 +1,22 @@
-const wrapperPlace = document.querySelector('#wrapper');
+const wrapperPlace = document.querySelector('#wrapper'),
+    arrColors = ['green', 'red'],
+    arrGamers = ['gamer1', 'gamer2'];
+
+class Gamer {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+    }
+
+}
+let Roman = new Gamer('Roman', 'green');
+
+createTableToWrapperPlace();
+
 
 function createTableToWrapperPlace() {
     const table = document.createElement('table');
+
     for (let i = 1; i < 30; i++) {
         let tr = document.createElement('tr');
         for (let j = 0; j < 30; j++) {
@@ -11,6 +26,21 @@ function createTableToWrapperPlace() {
         }
         wrapperPlace.appendChild(table);
     }
+    clickTable();
 }
 
-createTableToWrapperPlace();
+function clickTable() {
+    const tableGett = wrapperPlace.querySelector('table'),
+        tdGett = wrapperPlace.querySelectorAll('td');
+
+    tableGett.addEventListener('click', function(event) {
+
+        if (event.target.tagName == 'TD') {
+            event.target.style.backgroundColor = 'green';
+        }
+    });
+}
+
+function changeColors() {
+
+}
