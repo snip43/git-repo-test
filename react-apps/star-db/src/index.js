79f3c7a3831341b4bce94f8592d7ee4b9c7eaf53
snip43 <rko43@mail.ru>
2019-07-17 19:56:39 +0300

    11

--- react-apps/star-db/src/index.js
+++ react-apps/star-db/src/index.js
@@ -1,36 +1,13 @@
-export default class SwapiService {
-	_apiBase = 'https://swapi.co/api';
 
-	async getResourse(url){
-		const res = await fetch(`${this._apiBase}${url}`);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
-		if(!res.ok){
-			throw new Error(`Could not fetch ${url} , received ${res.status}`)
-		}
-		return await res.json();
-		}
-		async getAllPeople(){
-			const res = await this.getResourse(`/people/`);
-			return res.results;
-		}
-		getPerson(id){
-			return this.getResourse(`/people/${id}/`)
-		}
-		async getAllPlanets(){
-			const res = await this.getResourse(`/planets/`);
-			return res.results;
-		}
-		getPlanet(id){
-			return this.getResourse(`/planets/${id}/`)
-		}
-		async getAllStarships(){
-			const res = await this.getResourse(`/starships/`);
-			return res.results;
-		}
-		getStarship(id){
-			return this.getResourse(`/starships/${id}/`)
-		}
-}
+ReactDOM.render(<App />,
+  document.getElementById('root'));
+
+
+
 
 
 
