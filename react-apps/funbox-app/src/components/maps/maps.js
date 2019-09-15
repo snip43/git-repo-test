c8e8943992383200882a8c41227912571aede823
snip43 <rko43@mail.ru>
2019-09-15 10:41:06 +0300

    create react-yandex-maps

--- react-apps/funbox-app/src/components/maps/maps.js
+++ react-apps/funbox-app/src/components/maps/maps.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React  from 'react';
+import { YMaps, Map} from 'react-yandex-maps';
 import './maps.css';
 
-export default class YamapsApi extends Component {
+const YamapsApi = () => {
 	
-	render() {
 		return (
-			<div className = ''>
-				<a 
-				href="https://yandex.ru/maps/?um=constructor%3A52a2095c54e94f257dcce7bb3c50ed5a89a1481715b1818e4fc33292808371c9&amp;source=constructorStatic" 
-				> 
-					<img  className='img-fluid img-thumbnail'
-						src="https://api-maps.yandex.ru/services/constructor/1.0/static/?um=constructor%3A52a2095c54e94f257dcce7bb3c50ed5a89a1481715b1818e4fc33292808371c9&amp;lang=ru_RU" 
-						alt="" />
-				</a>
-			</div>	
+		
+			<YMaps>
+			
+				<Map defaultState={{ center: [55.75, 37.57], zoom: 10 }} />
+			
+			</YMaps>
+		
 		)
 	}
-}
+
+	export default YamapsApi;
