fa42c924af52f5e543c9d1bee8ae10608bd61e5c
snip43 <rko43@mail.ru>
2019-10-18 22:55:29 +0300

    refact

--- react-apps/it-kamasutra/kama-project-01/src/Navbar/Dialogs/Dialogs.js
+++ react-apps/it-kamasutra/kama-project-01/src/Navbar/Dialogs/Dialogs.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+
 import DialogItem from '../Dialog-item';
 import Message from '../Message/';
 
